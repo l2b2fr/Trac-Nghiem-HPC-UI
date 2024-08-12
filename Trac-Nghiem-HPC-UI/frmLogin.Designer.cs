7d00fb34db64e04f060d2df55d73602219202f79
Lê Minh Nam <108647533+l2b2fr@users.noreply.github.com>
2024-08-12 22:07:21 +0700

    SET-UP-PROJECT

---
+++ Trac-Nghiem-HPC-UI/frmLogin.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Trac_Nghiem_HPC_UI
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.directXFormContainerControl1 = new DevExpress.XtraEditors.DirectXFormContainerControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.imageCollectionFrmLogin = new DevExpress.Utils.ImageCollection(this.components);
+            this.htmlTemplateCollectionFrmLogin = new DevExpress.Utils.Html.HtmlTemplateCollection();
+            this.htmlMesage = new DevExpress.Utils.Html.HtmlTemplate();
+            this.directXFormContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollectionFrmLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // directXFormContainerControl1
+            // 
+            this.directXFormContainerControl1.Controls.Add(this.simpleButton1);
+            this.directXFormContainerControl1.Controls.Add(this.simpleButton2);
+            this.directXFormContainerControl1.Controls.Add(this.labelControl3);
+            this.directXFormContainerControl1.Controls.Add(this.textEdit2);
+            this.directXFormContainerControl1.Controls.Add(this.labelControl2);
+            this.directXFormContainerControl1.Controls.Add(this.labelControl1);
+            this.directXFormContainerControl1.Controls.Add(this.textEdit1);
+            this.directXFormContainerControl1.Location = new System.Drawing.Point(31, 81);
+            this.directXFormContainerControl1.Name = "directXFormContainerControl1";
+            this.directXFormContainerControl1.Size = new System.Drawing.Size(1000, 514);
+            this.directXFormContainerControl1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(88)))), ((int)(((byte)(163)))));
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Location = new System.Drawing.Point(98, 382);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(144, 44);
+            this.simpleButton1.TabIndex = 8;
+            this.simpleButton1.Text = "ĐĂNG NHẬP";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(88)))), ((int)(((byte)(163)))));
+            this.simpleButton2.Appearance.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton2.Appearance.Options.UseBackColor = true;
+            this.simpleButton2.Appearance.Options.UseFont = true;
+            this.simpleButton2.Location = new System.Drawing.Point(446, 382);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(144, 44);
+            this.simpleButton2.TabIndex = 7;
+            this.simpleButton2.Text = "THOÁT";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl3.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(88)))), ((int)(((byte)(163)))));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Appearance.Options.UseForeColor = true;
+            this.labelControl3.Location = new System.Drawing.Point(98, 244);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(135, 33);
+            this.labelControl3.TabIndex = 5;
+            this.labelControl3.Text = "MẬT KHẨU";
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(98, 283);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.AdvancedModeOptions.Label = "Nhập vào mật khẩu của bạn";
+            this.textEdit2.Properties.AdvancedModeOptions.LabelAppearance.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit2.Properties.AdvancedModeOptions.LabelAppearance.Options.UseFont = true;
+            this.textEdit2.Properties.Appearance.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit2.Properties.Appearance.Options.UseFont = true;
+            this.textEdit2.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.textEdit2.Properties.PasswordChar = '*';
+            this.textEdit2.Properties.UseAdvancedMode = DevExpress.Utils.DefaultBoolean.True;
+            this.textEdit2.Size = new System.Drawing.Size(492, 46);
+            this.textEdit2.TabIndex = 4;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(88)))), ((int)(((byte)(163)))));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Appearance.Options.UseForeColor = true;
+            this.labelControl2.Location = new System.Drawing.Point(98, 140);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(179, 33);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "MÃ SINH VIÊN";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Roboto", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(88)))), ((int)(((byte)(163)))));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Appearance.Options.UseForeColor = true;
+            this.labelControl1.Location = new System.Drawing.Point(98, 81);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(496, 42);
+            this.labelControl1.TabIndex = 2;
+            this.labelControl1.Text = "PHẦN MỀM THI TRẮC NGHIỆM";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.AllowHtmlTextInToolTip = DevExpress.Utils.DefaultBoolean.True;
+            this.textEdit1.Location = new System.Drawing.Point(98, 179);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.AdvancedModeOptions.AllowCaretAnimation = DevExpress.Utils.DefaultBoolean.True;
+            this.textEdit1.Properties.AdvancedModeOptions.Label = "Nhập mã sinh viên của bạn";
+            this.textEdit1.Properties.AdvancedModeOptions.LabelAppearance.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit1.Properties.AdvancedModeOptions.LabelAppearance.Options.UseFont = true;
+            this.textEdit1.Properties.AdvancedModeOptions.SelectionColor = System.Drawing.Color.White;
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Roboto", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.UseAdvancedMode = DevExpress.Utils.DefaultBoolean.True;
+            this.textEdit1.Size = new System.Drawing.Size(492, 46);
+            this.textEdit1.TabIndex = 0;
+            // 
+            // imageCollectionFrmLogin
+            // 
+            this.imageCollectionFrmLogin.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageCollectionFrmLogin.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollectionFrmLogin.ImageStream")));
+            this.imageCollectionFrmLogin.IsDpiAware = DevExpress.Utils.DefaultBoolean.True;
+            this.imageCollectionFrmLogin.Images.SetKeyName(0, "img_view.png");
+            this.imageCollectionFrmLogin.Images.SetKeyName(1, "logo.png");
+            // 
+            // htmlTemplateCollectionFrmLogin
+            // 
+            this.htmlTemplateCollectionFrmLogin.AddRange(new DevExpress.Utils.Html.HtmlTemplate[] {
+            this.htmlMesage});
+            // 
+            // htmlMesage
+            // 
+            this.htmlMesage.Name = "htmlMesage";
+            this.htmlMesage.Styles = resources.GetString("htmlMesage.Styles");
+            this.htmlMesage.Template = resources.GetString("htmlMesage.Template");
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ChildControls.Add(this.directXFormContainerControl1);
+            this.ClientSize = new System.Drawing.Size(1145, 624);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HtmlImages = this.imageCollectionFrmLogin;
+            this.HtmlTemplate.Styles = resources.GetString("frmLogin.HtmlTemplate.Styles");
+            this.HtmlTemplate.Template = resources.GetString("frmLogin.HtmlTemplate.Template");
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            this.HtmlElementMouseClick += new DevExpress.Utils.Html.DxHtmlElementMouseEventHandler(this.frmLogin_HtmlElementMouseClick);
+            this.directXFormContainerControl1.ResumeLayout(false);
+            this.directXFormContainerControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollectionFrmLogin)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.DirectXFormContainerControl directXFormContainerControl1;
+        private DevExpress.Utils.ImageCollection imageCollectionFrmLogin;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.Utils.Html.HtmlTemplateCollection htmlTemplateCollectionFrmLogin;
+        private DevExpress.Utils.Html.HtmlTemplate htmlMesage;
+    }
+}
---
+++ Trac-Nghiem-HPC-UI/frmLogin.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trac_Nghiem_HPC_UI
+{
+    public partial class frmLogin : DevExpress.XtraEditors.DirectXForm
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_HtmlElementMouseClick(object sender, DevExpress.Utils.Html.DxHtmlElementMouseEventArgs e)
+        {
+            if (e.ElementId == "point _red")
+            {
+                var arg = new XtraMessageBoxArgs();
+                arg.HtmlTemplate.Assign(htmlMesage);
+                arg.Caption = "Thông báo";
+                arg.Text = "Bạn có muốn thoát?";
+                arg.DefaultButtonIndex = 0;
+                var result = XtraMessageBox.Show(arg);
+                if (result == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            if (e.ElementId == "point _green")
+            {
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+    }
+}
